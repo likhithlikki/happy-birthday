4cdc4cbe6734cee8775d92b4cd0c72529c58aab3
likhith <123664920+likhithlikki@users.noreply.github.com>
2024-04-28 22:33:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var audioPlayer = document.getElementById("audioPlayer");
+var songSelect = document.getElementById("songSelect");
+
+// Set body opacity based on password
+let name = prompt("Enter your name: ");
+
+if (name === "chinni") {
+  document.querySelector("body").style.opacity = "100%";
+  alert("Thanks for confirming!");
+  audioPlayer.play();
+} else {
+  document.querySelector("body").style.opacity = "0%";
+  alert("Wrong password");
+  window.location.href = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ5CTzbRKH4kXv9nGYUvs501eiHk-3jUQXQhN5kP5Tr8JoTm_xJQLbhHiRHKGahkuY4sN1s";
+}
+
+function playSelectedSong() {
+    var selectedSong = songSelect.value;
+    audioPlayer.src = selectedSong;
+    audioPlayer.play();
+}
+
+audioPlayer.addEventListener("ended", function() {
+    playNextSong();
+});
+
+var songs = ["wannabe yours.mp3", "infinity_slowed.mp3", "nethone.mp3", "padi padileche manasu.mp3"];
+var currentIndex = 0; // Keep track of the current song index
+
+function playNextSong() {
+    currentIndex++;
+    if (currentIndex >= songs.length) {
+        currentIndex = 0;
+    }
+    audioPlayer.src = songs[currentIndex];
+    audioPlayer.play();
+}
+
+function changeImage() {
+  var image = document.getElementById('image');
+  var resolution = document.getElementById('resolution').value;
+  image.src = resolution;
+}
